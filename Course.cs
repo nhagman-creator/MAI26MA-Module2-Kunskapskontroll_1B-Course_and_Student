60e80911514600b4096432f74035584ebd6b9759
unknown <nhagman-creator>
2026-09-23 20:28:43 +0200

    Added main classes and code-structure

---
+++ Course.cs
@@ -0,0 +1,5 @@
+internal class Course //just for clarity (internal is already default according to google)
+{
+    private string Name = //just for clarity (private is already default)
+    internal Course () 
+}
